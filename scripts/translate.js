501b863b9ee2dc923b579967b547866f914e5c37
Jordan Jordanov <d_yugioh@abv.bg>
2018-06-14 21:36:20 +0300

    work

--- scripts/translate.js
+++ scripts/translate.js
@@ -1,3 +1,9 @@
 $("#translateToBulgarian").click(function() {
-  alert( "Handler for .click() called." );
+    let $jordanTitle = $("#name-title");
+    $jordanTitle.text("Йордан");
+
+    let $descriptiveParagraph = $("#descriptive-paragraph");
+    $descriptiveParagraph.text("Много амбициозен, креативен и бързообучаващ се софтуерен инженер с водещи умения, програмиране и архитектурни умения. Имам опит с ASP.NET MVC, NodeJS, PostgreSQL, SQL Server, MongoDB, HTML5, CSS3, Bootstrap, JSON, XML и познания в разработката на приложения с .NET платформа и MEAN стека. Също така разбиране на алгоритмични проблеми и структури от данни. Работи с прецизност и много добър отбор. Задължено да спазва сроковете, всички свои проекти са завършени навреме." );
+
+    toggleFunction();
 });
