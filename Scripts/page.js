08024aada59d4c5fca8c5c79af7ac7c0d712759c
Jordan Jordanov <d_yugioh@abv.bg>
2018-06-18 00:03:11 +0300

    code refactoring

--- Scripts/page.js
+++ Scripts/page.js
@@ -2,8 +2,8 @@
 function onClick(element) {
     document.getElementById("img01").src = element.src;
     document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
+  const captionText = document.getElementById('caption')
+  captionText.innerHTML = element.alt;
 }
 
 // Change style of navbar on scroll
@@ -12,8 +12,8 @@ window.onscroll = function () {
 };
 
 function myFunction() {
-    var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  const navbar = document.getElementById('myNavbar')
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
     } else {
         navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
@@ -22,8 +22,8 @@ function myFunction() {
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
+  const x = document.getElementById('navDemo')
+  if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
     } else {
         x.className = x.className.replace(" w3-show", "");
