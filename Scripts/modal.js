2ece1606c3b8f0ef045b6b1af4a71c9e138e415d
Jordan Jordanov <d_yugioh@abv.bg>
2018-07-01 09:56:12 +0300

    sunday morning commit

--- Scripts/modal.js
+++ Scripts/modal.js
@@ -1,56 +1,48 @@
-$( document ).ready(function() {
-  // Get the modal
-  var modal = document.getElementById('myModal');
+$(document).ready(function() {
+  const modal = document.getElementById('myModal');
+  const btn = document.getElementById('submitMailBtn');
+  const span = document.getElementsByClassName('close')[0];
 
-// Get the button that opens the modal
-  var btn = document.getElementById("submitMailBtn");
-
-// Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
+  span.onclick = function() {
     modal.style.display = "none";
   }
 
-// When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
+  window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
   }
 
-  $("#submitMailBtn").click(function () {
-    var replyToValue = $( "#replyTo" ).val();
-    var fromNameValue = $( "#fromName" ).val();
-    var messageHtmlValue = $( "#messageHtml" ).val();
+  $("#submitMailBtn").click(function() {
+    const replyToValue = $('#replyTo').val();
+    const fromNameValue = $('#fromName').val();
+    const messageHtmlValue = $('#messageHtml').val();
 
-    var serviceId = "myGmailService";
-    var templateId = "template_6BkEijYR";
-    var userId = "user_8yK59napehDiYt2SEfsf6";
+    const serviceId = 'myGmailService';
+    const templateId = 'template_6BkEijYR';
+    const userId = 'user_8yK59napehDiYt2SEfsf6';
+    const emailJsUrl = 'https://api.emailjs.com/api/v1.0/email/send';
 
-    var data = {
+    let data = {
       service_id: serviceId,
       template_id: templateId,
       user_id: userId,
       template_params: {
-        "reply_to": `${replyToValue}`,
-        "from_name": `${fromNameValue}`,
-        "message_html": `${messageHtmlValue}`
+        'reply_to': `${replyToValue}`,
+        'from_name': `${fromNameValue}`,
+        'message_html': `${messageHtmlValue}`
       }
-    };
+    }
 
-    console.table(data);
-    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+    $.ajax(emailJsUrl, {
       type: 'POST',
       data: JSON.stringify(data),
       contentType: 'application/json'
-    }).done(function () {
-    modal.style.display='block';
-    }).fail(function (error) {
+    }).done(function() {
+      modal.style.display = 'block';
+    }).fail(function(error) {
       alert('Oops... ' + JSON.stringify(error));
     });
   });
 
 });
-
