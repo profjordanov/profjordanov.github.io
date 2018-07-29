e662705956752cc7a1bfe9be7f879c5c62ac171d
Jordan Jordanov <d_yugioh@abv.bg>
2018-07-29 19:12:03 +0300

    refactoring

--- Scripts/analytics.js
+++ Scripts/analytics.js
@@ -1,8 +1,6 @@
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'UA-119896407-1');
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'UA-119896407-1');
